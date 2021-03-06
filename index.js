12aeac74387e893e47b1656ba1186a237c48c9ad
Robin Grabs <robinmgrabs@gmail.com>
2021-03-06 09:05:09 +0100

    packages set up, modules added to git ignore.

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+console.log('all good!');
