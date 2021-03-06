f53d31bf94be5987a72ab084f1765ff62c4583d8
Robin Grabs <robinmgrabs@gmail.com>
2021-03-06 17:37:27 +0100

    error handling for ooc endpoints.

--- ooc_webscraper.js
+++ ooc_webscraper.js
@@ -1,1 +1,6 @@
-module.exports = (pageCount, searchTerm) => {};
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const oocBaseUrl = 'https://outof.cards/page=';
+
+module.exports = (resultLimit, query) => {};
--- routes/scraping.js
+++ routes/scraping.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const validEndpoints = ['query', 'limit', 'fulltext'];
+
 router.get('/', (req, res) =>
 	res.json({ msg: "Here you'll find some scraping being done." })
 );
 
 router.get('/ooc', (req, res) =>
-	res.json({ msg: 'Get the home page scraped for custom news.' })
+	res.json({ msg: 'Get the home page of ooc scraped for custom news.' })
 );
 
 const oocWebScraper = require('../ooc_webscraper');
 router.get('/ooc/:query', (req, res) => {
+	let requestOK = true;
+
+	//* Break out endpoints
 	const args = req.params.query.split('&').map((arg) => {
 		const endpoint = arg.split('=')[0];
 		const value = arg.split('=')[1];
 		return { endpoint, value };
 	});
-	console.log(args);
-	let json = oocWebScraper(...args);
 
-	res.json(args);
+	//* Check validity of endpoints
+	args.forEach((arg) => {
+		if (requestOK && !validEndpoints.includes(arg.endpoint)) {
+			res
+				.status(400)
+				.json({ status: 400, msg: `Endpoint '${arg.endpoint}' is invalid.` });
+			requestOK = false;
+			return;
+		}
+	});
+	if (!requestOK) {
+		return;
+	}
+	console.log(args);
+	//* Scrape with provided endpoints and return
+	let json = oocWebScraper(args);
+	res.status(200).json({ status: 200, response: args });
 });
 
 module.exports = router;
